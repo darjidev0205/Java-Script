53664b5e76771abfaceba8f3ca911c99ecad157e
Dev Darji <devbhikhabhaidarji@gmail.com>
2026-04-18 05:10:21 +0000

    data types

---
+++ basic_01/dataTypes_02.js
@@ -0,0 +1,27 @@
+"use strict" // treat all js code  as newer  version
+
+//alert (3 + 3) => we are using Node.js not browser so its show error
+
+ console.log (3 + 
+    
+    3) //code readability should be high
+ console.log("Darji Dev")
+
+ let name = "DEV"
+ let age = 18
+ let isLoggedIn = false
+ let state;
+
+ // number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // type of undifined is undefined
+console.log(typeof null); // type of null is object => it's important for interview
