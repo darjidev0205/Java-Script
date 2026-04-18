faf1b75204b6bcdbbadb9b9fb07959c7e335866e
Dev Darji <devbhikhabhaidarji@gmail.com>
2026-04-18 04:43:36 +0000

    let const var learning

---
+++ basic_01/variable_01.js
@@ -0,0 +1,20 @@
+const accountId = 2
+let accountEmail = "darjidev@gmail.com"
+var accountPassword = "darji dev"
+accountCity = "Gandhinagar"
+
+// its print single single output in new line
+/*console.log(accountPassword);
+console.log(accountEmail);
+console.log(accountPassword);
+console.log(accountCity);
+*/
+
+/*
+prefer not use var
+because of issue in block scope and functional scope
+*/
+
+//when we use table instead of loog its print table type data 
+
+console.table([accountId,accountEmail,accountPassword,accountCity]);
